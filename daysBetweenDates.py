10ab36c832e1f33fafa43fc69bd420be14aa03ef
Rachit Pareek <pareekrachit@gmail.com>
2015-01-15 23:55:17 +0530

    Create daysBetweenDates.py

---
+++ daysBetweenDates.py
@@ -0,0 +1,63 @@
+def leapyear(year):
+    if(year % 4 == 0):
+        if(year % 100 == 0):
+            if(year % 400 == 0):
+                return True
+            else:    
+                return False
+        else:
+            return True
+    else:
+        return False
+
+def daysInmonth(year, month):
+    
+    if leapyear(year):
+        d = [31,29,31,30,31,30,31,31,30,31,30,31] 
+        return d[month-1]
+    else:
+        d = [31,28,31,30,31,30,31,31,30,31,30,31]
+        return d[month-1]
+        
+def nextDay(year, month, day):
+    if day < daysInmonth(year, month):
+        return year, month, day + 1
+    else:
+        if month == 12:
+            return year + 1, 1, 1
+        else:
+            return year, month + 1, 1
+
+def dateIsBefore(year1, month1, day1, year2, month2, day2):
+    if year1 < year2:
+        return True
+    if year1 == year2:
+        if month1 < month2:
+            return True
+        if month1 == month2:
+            return day1 < day2
+    return False        
+        
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    assert not dateIsBefore(year2, month2, day2, year1, month1, day1)
+    days = 0
+    while dateIsBefore(year1, month1, day1, year2, month2, day2):
+        year1, month1, day1 = nextDay(year1, month1, day1)
+        days += 1
+    return days
+
+def test():
+    test_cases = [((2012,1,1,2012,2,28), 58), 
+                  ((2012,1,1,2012,3,1), 60),
+                  ((2011,6,30,2012,6,30), 366),
+                  ((2011,1,1,2012,8,8), 585 ),
+                  ((1900,1,1,1999,12,31), 36523)]
+    
+    for (args, answer) in test_cases:
+        result = daysBetweenDates(*args)
+        if result != answer:
+            print "Test with data:", args, "failed"
+        else:
+            print "Test case passed!"
+
+test()
